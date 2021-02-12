bbe2ae71c0209d5fb049f9f4c74792629a98264a
Nicola <venuti@amazon.com>
2021-02-12 18:11:27 +0000

    Fix chown ssh key in the bootstrap script

--- scripts/Cloud9-Bootstrap.sh
+++ scripts/Cloud9-Bootstrap.sh
@@ -8,7 +8,7 @@ cd /home/ec2-user/environment
 sudo yum -y install jq
 pip install --user -U boto boto3 botocore awscli aws-sam-cli aws-parallelcluster
 aws ec2 create-key-pair --key-name ${KEY_PAIR} --query KeyMaterial --output text > /home/ec2-user/.ssh/id_rsa
-chown 644 /home/ec2-user/.ssh/id_rsa
+sudo chown 644 /home/ec2-user/.ssh/id_rsa
 wget https://raw.githubusercontent.com/aws-samples/aws-pcluster-post-samples/development/parallelcluster/${PC_CONFIG}
 /usr/bin/envsubst < ${PC_CONFIG} > cluster.config
 sudo chown -R ec2-user:ec2-user /home/ec2-user/
