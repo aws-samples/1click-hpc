17061e8b56d89462132dd1b92926fa1c4e838f41
Nicola <venuti@amazon.com>
2021-03-16 12:04:01 +0000

    Module to configure DCV pat-url (for the ALB)

---
+++ scripts/26.configure.dcv.alb.compute.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the "Software"), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify,
+# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+# Intall DCV con compute Nodes.
+export SLURM_CONF_FILE="/opt/slurm/etc/pcluster/slurm_parallelcluster_*_partition.conf"
+export DCV_KEY_WORD=$(jq --arg default "dcv" -r '.post_install.dcv | if has("dcv_queue_keyword") then .dcv_queue_keyword else $default end' "${dna_json}")
+
+set -x
+set -e
+
+configureDCVforALB() {
+    cp '/etc/dcv/dcv.conf' "/etc/dcv/dcv.conf.$(date --iso=s --utc)"
+    WEB_URL_PATH="$(ec2-metadata -i| awk '{print $2}')"
+    pattern='^ *#web-url-path*=.*$'
+    replace="web-url-path=\"${WEB_URL_PATH}\""
+    sed -i -e "s|${pattern}|${replace}|" "/etc/dcv/dcv.conf"
+}
+
+
+# main
+# ----------------------------------------------------------------------------
+main() {
+    echo "[INFO][$(date '+%Y-%m-%d %H:%M:%S')] configure.dcv.alb.compute.sh: START" >&2
+
+    for conf_file in $(ls ${SLURM_CONF_FILE} | grep "${DCV_KEY_WORD}"); do
+        if [[ ! -z $(grep "${compute_instance_type}" "${conf_file}") ]]; then
+            configureDCVforALB
+        fi
+    done
+        
+    echo "[INFO][$(date '+%Y-%m-%d %H:%M:%S')] configure.dcv.alb.compute.sh: STOP" >&2
+}
+
+main "$@"
