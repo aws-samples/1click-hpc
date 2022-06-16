4b2f323b95ece87a8094b11f569c87537524d0e3
Richard Vencu <vencu.richard@gmail.com>
2022-06-16 16:22:29 +0300

    try ODCR fix 1

--- modules/50.install.capacity.reservation.pool.sh
+++ modules/50.install.capacity.reservation.pool.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 set -e
 
+ACCOUNT_ID=`aws sts get-caller-identity | jq -r '."Account"'`
+EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
+EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed 's/[a-z]$//'`"
+
 # Override run_instance attributes
+# Name of the group is still hardcoded, need a way to get variable from cloudformation here
 cat > /opt/slurm/etc/pcluster/run_instances_overrides.json << EOF
 {
-    "compute-od-1": {
+    "compute-od-gpu": {
         "p4d-24xlarge": {
             "CapacityReservationSpecification": {
                 "CapacityReservationTarget": {
-                    "CapacityReservationResourceGroupArn": "arn:aws:resource-groups:us-east-1:842865360552:group/EC2CRGroup"
+                    "CapacityReservationResourceGroupArn": "arn:aws:resource-groups:$EC2_REGION:$ACCOUNT_ID:group/EC2CRGroup"
                 }
             }
         }
