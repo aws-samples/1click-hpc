f14f965a2f28b2a87b7ae5dcd2c9edf7a2a0afa4
Richard Vencu <vencu.richard@gmail.com>
2022-06-17 16:46:38 +0300

    fix path system wide

--- modules/70.install.singularity.sh
+++ modules/70.install.singularity.sh
@@ -2,16 +2,10 @@
 # Install basic tools for compiling
 yum groupinstall -y 'Development Tools'
 # Install RPM packages for dependencies
-yum install -y \
-   wget \
-   libseccomp-devel \
-   glib2-devel \
-   squashfs-tools \
-   cryptsetup \
-   runc
+yum install -y wget libseccomp-devel glib2-devel squashfs-tools cryptsetup runc
 wget https://go.dev/dl/go1.18.3.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.3.linux-amd64.tar.gz
-echo 'export PATH=$PATH:/usr/local/go/bin' >> /home/ec2-user/.bashrc
+echo 'export PATH=/usr/local/go/bin:$PATH' >> /etc/profile.d/singularity.sh
 
 wget https://github.com/sylabs/singularity/releases/download/v3.10.0/singularity-ce-3.10.0-1.el7.x86_64.rpm
 rpm -i singularity-ce-3.10.0-1.el7.x86_64.rpm
