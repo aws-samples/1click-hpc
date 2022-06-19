44ebbd09f734bd969a7b268002f95239d5949af0
Richard Vencu <vencu.richard@gmail.com>
2022-06-19 13:09:50 +0300

    create custom namespace for CloudWatch

--- modules/60.install.gpumon.cloudwatch.metrics.sh
+++ modules/60.install.gpumon.cloudwatch.metrics.sh
@@ -9,6 +9,7 @@ git clone https://github.com/Stability-AI/gpumon-service-for-cloudwatch.git
 yum install -y python3.8 python3-distutils
 python3.8 -m pip install -r gpumon-service-for-cloudwatch/requirements.txt
 
+sed -i 's/GPUMonitoring/${CLUSTER_NAME}-GPU-Monitoring/' gpumon-service-for-cloudwatch/gpumon.py
 mv gpumon-service-for-cloudwatch/gpumon.py /etc/gpumon.py
 mv gpumon-service-for-cloudwatch/gpumon.service /etc/systemd/system/gpumon.service
 
