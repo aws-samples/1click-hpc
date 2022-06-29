72610c1b3b2433fa8cb1120ea5dc67843d54d3c1
Richard Vencu <vencu.richard@gmail.com>
2022-06-29 17:23:07 +0300

    fix LD_PATH missing EFA folders

--- modules/45.install.nccl.compute.sh
+++ modules/45.install.nccl.compute.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo -e '#!/bin/sh\nexport LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/amazon/openmpi/lib64:/opt/amazon/efa/lib64\nexport PATH=$PATH:/opt/amazon/efa/bin:/opt/amazon/openmpi/bin' | sudo tee /etc/profile.d/amazon_efa.sh
+
 cd /opt
 sudo git clone https://github.com/NVIDIA/nccl.git
 cd nccl
