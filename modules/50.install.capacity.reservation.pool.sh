cf3680de8a8998f6ddb448c738bb12a186d24167
Richard Vencu <vencu.richard@gmail.com>
2022-07-01 11:36:03 +0300

    fix configs

--- modules/50.install.capacity.reservation.pool.sh
+++ modules/50.install.capacity.reservation.pool.sh
@@ -13,6 +13,15 @@ cat > /opt/slurm/etc/pcluster/run_instances_overrides.json << EOF
                 }
             }
         }
+    },
+    "compute-od-mig": {
+        "p4d-24xlarge": {
+            "CapacityReservationSpecification": {
+                "CapacityReservationTarget": {
+                    "CapacityReservationResourceGroupArn": "arn:aws:resource-groups:${AWS_REGION_NAME}:${AWS_ACCOUNT}:group/${CLUSTER_NAME}-ODCR-Group"
+                }
+            }
+        }
     }
 }
 EOF
