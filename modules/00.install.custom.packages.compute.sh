c620dabb8bec081a0d7449956349cfedd8bd29a4
Richard Vencu <vencu.richard@gmail.com>
2022-07-05 00:49:56 +0300

    add jupyter automation

--- modules/00.install.custom.packages.compute.sh
+++ modules/00.install.custom.packages.compute.sh
@@ -2,5 +2,5 @@
 set -e
 
 amazon-linux-extras enable python3.8
-yum install wget python38 glances htop hwloc iftop kernel-tools numactl python3-devel python38-devel kernel-devel check check-devel subunit subunit-devel -y
+yum install wget tmux python38 glances htop hwloc iftop kernel-tools numactl python3-devel python38-devel kernel-devel check check-devel subunit subunit-devel -y
 yum groupinstall -y 'Development Tools'
--- tests/jupyter.sh
+++ tests/jupyter.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 cat > jupyter.batch << EOF
 #!/bin/bash
 #SBATCH --job-name=jupyter
@@ -7,6 +9,29 @@ cat > jupyter.batch << EOF
 #SBATCH --output=%x_%j.out
 
 cat /etc/hosts
-jupyter lab --ip=0.0.0.0 --port=8888
+python3.8 -m pip install notebook
+jupyter notebook --ip=0.0.0.0 --port=8888
 EOF
 
+cat > py_control.py << EOF
+import os
+
+# get slurm job ID
+jobId = os.popen('sbatch jupyter.batch').read()
+jobId = [int(s) for s in txt.split() if s.isdigit()][0]
+
+# wait until notebook server is started
+os.system('( tail -f -n0 jupyter_$jobId & ) | grep -q "http://127.0.0.1"')
+
+# extract compute node IP
+jIP = '172.31.0.0'
+
+# extract jupyter notebook token
+token = 5a8e2f96e15ef2113d326b02eeb3dc5b83aaba65eaa3b736
+
+echo "ssh with 'ssh -L8888:$jIP:8888 ec2-user@52.71.232.47'"
+
+echo "then visit http://127.0.0.1:8888/?token=$token"
+EOF
+
+python3 py_control.py
