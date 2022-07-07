58e35b53529310efebab2fe1004ff3da6a2a5e94
Richard Vencu <vencu.richard@gmail.com>
2022-07-07 16:25:23 +0300

    remove new postinstall scripts

--- modules/02.activate.passwordless.AD.headnode.sh
+++ modules/02.activate.passwordless.AD.headnode.sh
@@ -2,7 +2,9 @@
 set -e
 
 sed -i '0,/use_fully_qualified_names = False$/s//use_fully_qualified_names = False\nldap_user_extra_attrs = altSecurityIdentities\nldap_user_ssh_public_key = altSecurityIdentities/' /etc/sssd/sssd.conf
-systemctl stop sssd; rm -rf /var/lib/sss/{db,mc}/*; systemctl start sssd
+#systemctl stop sssd
+#rm -rf /var/lib/sss/{db,mc}/*
+#systemctl start sssd
 echo "AuthorizedKeysCommand /usr/bin/sss_ssh_authorizedkeys" >> /etc/ssh/sshd_config
 echo "AuthorizedKeysCommandUser root" >> /etc/ssh/sshd_config
 systemctl restart sshd
