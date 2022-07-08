d3b8541aa413973d7013e8edc49fb8f718d36e13
Richard Vencu <vencu.richard@gmail.com>
2022-07-08 21:01:37 +0000

    try another fix

--- scripts/Cloud9-Bootstrap.sh
+++ scripts/Cloud9-Bootstrap.sh
@@ -50,9 +50,8 @@ sudo cp /etc/resolv.conf.OK /etc/resolv.conf
 sudo amazon-linux-extras install -y lustre2.10 > /dev/null 2>&1
 
 # workaround dependency issue
-python3 -m pip install aws-cdk.aws-ecs==1.155.0
-python3 -m pip install aws-cdk.aws-route53-targets==1.155.0
-python3 -m pip install aws-cdk.aws-batch==1.155.0
+python3 -m pip install -U pip
+python3 -m pip install -U setuptools
 # end workaround
 python3 -m pip install "aws-parallelcluster" --upgrade --user --quiet
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
