ca51cf0236ab4784c2f8f9f001f91a8e0f1dc60b
Richard Vencu <vencu.richard@gmail.com>
2022-07-10 08:14:31 +0000

    fix nccl version

--- modules/45.install.nccl.compute.sh
+++ modules/45.install.nccl.compute.sh
@@ -4,10 +4,10 @@ set -e
 echo -e '#!/bin/sh\nexport LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/amazon/openmpi/lib64:/opt/amazon/efa/lib64\nexport PATH=$PATH:/opt/amazon/efa/bin:/opt/amazon/openmpi/bin' | sudo tee /etc/profile.d/amazon_efa.sh
 
 cd /opt
-sudo git clone https://github.com/NVIDIA/nccl.git
+git clone https://github.com/NVIDIA/nccl.git
 cd nccl
-sudo git checkout {{user `nccl_version`}}
-sudo make -j src.build CUDA_HOME=/usr/local/cuda NVCC_GENCODE='-gencode=arch=compute_70,code=sm_70 -gencode=arch=compute_75,code=sm_75 -gencode=arch=compute_80,code=sm_80'
+git checkout 'v2.12.7-1'
+make -j src.build CUDA_HOME=/usr/local/cuda NVCC_GENCODE='-gencode=arch=compute_70,code=sm_70 -gencode=arch=compute_75,code=sm_75 -gencode=arch=compute_80,code=sm_80'
 echo -e '#!/bin/sh\nexport LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/nccl/build/lib\nexport NCCL_PROTO=simple' | sudo tee /etc/profile.d/nccl.sh
 
 cd /opt
@@ -15,7 +15,7 @@ git clone https://github.com/aws/aws-ofi-nccl.git -b aws
 cd aws-ofi-nccl
 export PATH=$PATH:/opt/amazon/efa/bin:/opt/amazon/openmpi/bin
 ./autogen.sh
-sudo mkdir -p /opt/aws-ofi-nccl
+mkdir -p /opt/aws-ofi-nccl
 ./configure --prefix=/opt/aws-ofi-nccl --with-mpi=/opt/amazon/openmpi --with-libfabric=/opt/amazon/efa --with-nccl=/opt/nccl/build --with-cuda=/usr/local/cuda
 make && sudo make install
 echo -e '#!/bin/sh\nexport LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/aws-ofi-nccl/lib' | sudo tee /etc/profile.d/aws-ofi-nccl.sh
