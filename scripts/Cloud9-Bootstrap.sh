fbb5e58a50700a76bb8d172348e0f2ce6ddc7db1
Richard Vencu <vencu.richard@gmail.com>
2022-07-12 09:44:09 +0000

    more fixes to try

--- scripts/Cloud9-Bootstrap.sh
+++ scripts/Cloud9-Bootstrap.sh
@@ -159,8 +159,8 @@ sudo bash -c "echo \"$FSX_DNS_NAME@tcp:/$FSX_MOUNT_NAME /home/ec2-user/environme
 sudo chmod 755 fsx
 sudo chown ec2-user:ec2-user fsx
 
-aws s3 cp --quiet bootstrap.log "s3://${S3_BUCKET}/install.log" --region "${AWS_REGION_NAME}"
-aws s3 cp --quiet config.${AWS_REGION_NAME}.yaml "s3://${S3_BUCKET}/config.${AWS_REGION_NAME}.yaml" --region "${AWS_REGION_NAME}"
+#aws s3 cp --quiet bootstrap.log "s3://${S3_BUCKET}/install.log" --region "${AWS_REGION_NAME}"
+#aws s3 cp --quiet config.${AWS_REGION_NAME}.yaml "s3://${S3_BUCKET}/config.${AWS_REGION_NAME}.yaml" --region "${AWS_REGION_NAME}"
 
 # send SUCCESFUL to the wait handle
 curl -X PUT -H 'Content-Type:' \
