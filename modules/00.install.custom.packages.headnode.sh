b44e27660901961d281537d39ed58f98ca5fcb71
Richard Vencu <vencu.richard@gmail.com>
2022-07-13 06:55:53 +0000

    optimize for large jobs

--- modules/00.install.custom.packages.headnode.sh
+++ modules/00.install.custom.packages.headnode.sh
@@ -3,3 +3,7 @@ set -e
 
 amazon-linux-extras enable python3.8
 yum install -y python38 python38-devel tmux htop glances aria2 transmission-cli
+
+# optimize for large jobs
+ifconfig eth0 txqueuelen 4096
+sed -i -e '/MessageTimeout=/ s/= .*/= 180/' /opt/slurm/etc/slurm.conf
