0b6a02a09a3808e7d6db05f4638d21e4f90c6892
Richard Vencu <vencu.richard@gmail.com>
2022-07-26 08:21:12 +0000

    fix up and down

--- scripts/taskepilog.sh
+++ scripts/taskepilog.sh
@@ -15,9 +15,9 @@ if [[ $SLURM_JOB_NAME == M\ * ]]; then
     compute_instance_type=$(curl http://169.254.169.254/latest/meta-data/instance-type)
     gpu_instances="[pg][2-9].*\.[0-9]*[x]*large"
     if [[ $compute_instance_type =~ $gpu_instances ]]; then
-        /usr/local/bin/docker-compose -f "${monitoring_home}/docker-compose/docker-compose.compute.gpu.yml" -p monitoring-compute up -d
+        /usr/local/bin/docker-compose -f "${monitoring_home}/docker-compose/docker-compose.compute.gpu.yml" -p monitoring-compute down
     else
-        /usr/local/bin/docker-compose -f "${monitoring_home}/docker-compose/docker-compose.compute.yml" -p monitoring-compute up -d
+        /usr/local/bin/docker-compose -f "${monitoring_home}/docker-compose/docker-compose.compute.yml" -p monitoring-compute down
     fi
 
 fi
--- scripts/taskprolog.sh
+++ scripts/taskprolog.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-#FIXME: do not hardcode.
-export SHARED_FS_DIR="/fsx"
-monitoring_dir_name="monitoring"
-prefix="parallelcluster-"
-string=${SLURM_CLUSTER_NAME}
-#remove prefix from string
-stack_name=${string#"$prefix"}
-monitoring_home="${SHARED_FS_DIR}/${monitoring_dir_name}/${stack_name}"
+if [[ $SLURM_JOB_NAME == M\ * ]]; then
 
-#get compute instance type
-compute_instance_type=$(curl http://169.254.169.254/latest/meta-data/instance-type)
-gpu_instances="[pg][2-9].*\.[0-9]*[x]*large"
-if [[ $compute_instance_type =~ $gpu_instances ]]; then
-    /usr/local/bin/docker-compose -f "${monitoring_home}/docker-compose/docker-compose.compute.gpu.yml" -p monitoring-compute up -d
-else
-    /usr/local/bin/docker-compose -f "${monitoring_home}/docker-compose/docker-compose.compute.yml" -p monitoring-compute up -d
+    #FIXME: do not hardcode.
+    export SHARED_FS_DIR="/fsx"
+    monitoring_dir_name="monitoring"
+    prefix="parallelcluster-"
+    string=${SLURM_CLUSTER_NAME}
+    #remove prefix from string
+    stack_name=${string#"$prefix"}
+    monitoring_home="${SHARED_FS_DIR}/${monitoring_dir_name}/${stack_name}"
+
+    #get compute instance type
+    compute_instance_type=$(curl http://169.254.169.254/latest/meta-data/instance-type)
+    gpu_instances="[pg][2-9].*\.[0-9]*[x]*large"
+    if [[ $compute_instance_type =~ $gpu_instances ]]; then
+        /usr/local/bin/docker-compose -f "${monitoring_home}/docker-compose/docker-compose.compute.gpu.yml" -p monitoring-compute up -d
+    else
+        /usr/local/bin/docker-compose -f "${monitoring_home}/docker-compose/docker-compose.compute.yml" -p monitoring-compute up -d
+    fi
+
 fi
