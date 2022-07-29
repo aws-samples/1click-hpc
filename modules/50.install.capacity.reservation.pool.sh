1a980bd99f88f728b5238d38bcf94005efbf4f26
Richard Vencu <vencu.richard@gmail.com>
2022-07-29 08:30:38 +0000

    fix capacity partitions

--- modules/50.install.capacity.reservation.pool.sh
+++ modules/50.install.capacity.reservation.pool.sh
@@ -7,7 +7,7 @@ installODCR() {
     # Name of the group is still hardcoded, need a way to get variable from cloudformation here
     cat > /opt/slurm/etc/pcluster/run_instances_overrides.json << EOF
 {
-    "compute-od-gpu": {
+    "gpu": {
         "p4d-24xlarge": {
             "CapacityReservationSpecification": {
                 "CapacityReservationTarget": {
@@ -16,7 +16,7 @@ installODCR() {
             }
         }
     },
-    "compute-od-mig": {
+    "gpu-mig": {
         "p4d-24xlarge": {
             "CapacityReservationSpecification": {
                 "CapacityReservationTarget": {
@@ -25,7 +25,6 @@ installODCR() {
             }
         }
     },
-    "compute-od-jupyter": {
         "p4d-24xlarge": {
             "CapacityReservationSpecification": {
                 "CapacityReservationTarget": {
