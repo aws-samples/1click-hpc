43be9101ed4f387e39776c29982dfe38145f63fc
Richard Vencu <vencu.richard@gmail.com>
2022-08-13 22:59:03 +0000

    fix sssd at compute nodes

--- modules/02.activate.passwordless.sudo.compute.sh
+++ modules/02.activate.passwordless.sudo.compute.sh
@@ -2,6 +2,15 @@
 set -x
 set -e
 
+activateSSSD() {
+    sed -i '0,/use_fully_qualified_names = False$/s//use_fully_qualified_names = False\nldap_user_extra_attrs = altSecurityIdentities\nldap_user_ssh_public_key = altSecurityIdentities/' /etc/sssd/sssd.conf
+    #sed -i '/^ldap_search_base/ s/$/?subtree?(&(!(objectClass=computer))(!(userAccountControl:1.2.840.113556.1.4.803:=2)))/' /etc/sssd/sssd.conf
+    sed -i '0,/^[domain/default]/a enumerate = true' /etc/sssd/sssd.conf
+    systemctl stop sssd
+    rm -rf /var/lib/sss/{db,mc}/*
+    systemctl start sssd
+}
+
 addAdmins2Sudoers() {
     cat > /etc/sudoers.d/100-AD-admins << EOF
 # add domain admins as sudoers
@@ -9,12 +18,21 @@ addAdmins2Sudoers() {
 EOF
 }
 
+setupCron(){
+    # Configure the script to run every minute
+    echo "
+*/10 * * * * systemctl stop sssd; rm -rf /var/lib/sss/{db,mc}/*; systemctl start sssd
+" | crontab -
+}
+
 
 # main
 # ----------------------------------------------------------------------------
 main() {
     echo "[INFO][$(date '+%Y-%m-%d %H:%M:%S')] 02.activate.passwordless.sudo.compute.sh: START" >&2
+    activateSSSD
     addAdmins2Sudoers
+    setupCron
     echo "[INFO][$(date '+%Y-%m-%d %H:%M:%S')] 02.activate.passwordless.sudo.compute.sh: STOP" >&2
 }
 
