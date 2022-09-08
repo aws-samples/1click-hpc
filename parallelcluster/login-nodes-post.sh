d58a90d2e887240616b55c69aa748e9bc7c1a07f
Richard Vencu <vencu.richard@gmail.com>
2022-09-08 12:40:57 +0000

    more verbosity

--- parallelcluster/login-nodes-post.sh
+++ parallelcluster/login-nodes-post.sh
@@ -2,6 +2,7 @@
 
 # use the ideas from https://swsmith.cc/posts/slurm-login-aws-parallelcluster.html
 # make an AMI from the headnode (172.31.36.138)
+# then spin up a new instance with that AMI and do:
 
 systemctl disable slurmctld
 systemctl disable slurmdbd
@@ -18,6 +19,8 @@ echo "172.31.36.138:/home /home nfs hard,_netdev,noatime 0 2" >> /etc/fstab
 echo "172.31.36.138:/opt/slurm /opt/slurm nfs hard,_netdev,noatime 0 2" >> /etc/fstab
 mount -a
 
+# note: not all volumes mount at boot. A mount -a command post reboot solves the problem.
+# add on crontab as temporary fix this line:
 line = "@reboot sleep 30 && mount -a"
 (crontab -u $(whoami) -l; echo "$line" ) | crontab -u $(whoami) -
 
@@ -52,7 +55,5 @@ systemctl start slurmd.service
 
 # now logoff and make a new AMI from this node image
 # use the new AMI to spin up multiple login nodes
-
-# note: not all volumes mount at boot. A mount -a command post reboot solves the problem.
-# add on crontab as temporary fix this line:
-# @reboot sleep 30 && mount -a
+# can also deregister the initial headnode AMI to save on expenses
+# do not forget to manually tag the new login nodes with relevant tags 
