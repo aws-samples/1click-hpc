d83815393d0fa7093c6d95adc715a590c3779b40
Richard Vencu <vencu.richard@gmail.com>
2022-09-26 20:32:59 +0000

    sssd config in cluster config

--- modules/02.activate.passwordless.AD.headnode.sh
+++ modules/02.activate.passwordless.AD.headnode.sh
@@ -3,15 +3,16 @@ set -x
 set -e
 
 activateSSH() {
-    sed -i '0,/use_fully_qualified_names = False$/s//use_fully_qualified_names = False\nldap_user_extra_attrs = altSecurityIdentities\nldap_user_ssh_public_key = altSecurityIdentities/' /etc/sssd/sssd.conf
+    #sed -i '0,/use_fully_qualified_names = False$/s//use_fully_qualified_names = False\nldap_user_extra_attrs = altSecurityIdentities\nldap_user_ssh_public_key = altSecurityIdentities/' /etc/sssd/sssd.conf
     #sed -i '/^ldap_search_base/ s/$/?subtree?(&(!(objectClass=computer))(!(userAccountControl:1.2.840.113556.1.4.803:=2)))/' /etc/sssd/sssd.conf
-    sed -i '0,/^[domain/default]/a enumerate = true' /etc/sssd/sssd.conf
+    #sed -i '0,/^[domain/default]/a enumerate = true' /etc/sssd/sssd.conf
     sed -i 's/fallback_homedir = \/home\/%u/override_homedir = \/fsx\/home-%u/g' /etc/sssd/sssd.conf
-    systemctl stop sssd
-    rm -rf /var/lib/sss/{db,mc}/*
-    systemctl start sssd
+    #systemctl stop sssd
+    #rm -rf /var/lib/sss/{db,mc}/*
+    systemctl restart sssd
     echo "AuthorizedKeysCommand /usr/bin/sss_ssh_authorizedkeys" >> /etc/ssh/sshd_config
     echo "AuthorizedKeysCommandUser root" >> /etc/ssh/sshd_config
+    sed -E -i 's|^#?(PasswordAuthentication)\s.*|\1 no|' /etc/ssh/sshd_config
     systemctl restart sshd
 }
 
@@ -21,11 +22,6 @@ addAdmins2Sudoers() {
 # add domain admins as sudoers
 %Sudoers  ALL=(ALL) NOPASSWD:ALL
 EOF
-}
-
-removePasswordAuth(){
-    sed -E -i 's|^#?(PasswordAuthentication)\s.*|\1 no|' /etc/ssh/sshd_config
-    systemctl restart sshd
 }
 
 setupCron(){
@@ -42,7 +38,6 @@ main() {
     echo "[INFO][$(date '+%Y-%m-%d %H:%M:%S')] 02.activate.passwordless.AD.headnode.sh: START" >&2
     activateSSH
     addAdmins2Sudoers
-    #removePasswordAuth
     #setupCron
     echo "[INFO][$(date '+%Y-%m-%d %H:%M:%S')] 02.activate.passwordless.AD.headnode.sh: STOP" >&2
 }
