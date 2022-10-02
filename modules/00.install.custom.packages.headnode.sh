d48ecf164037bbfc5c9347bbd7675ffc28f4ecff
Richard Vencu <vencu.richard@gmail.com>
2022-10-02 16:27:17 +0000

    install custom packages later

--- modules/00.install.custom.packages.headnode.sh
+++ modules/00.install.custom.packages.headnode.sh
@@ -5,7 +5,7 @@ set -e
 installCustom() {
     amazon-linux-extras enable python3.8
 
-    #yum -y update
+    yum -y update
     yum install -y python38 python38-devel tmux htop iftop transmission-cli pssh pango-devel cairo-devel tokyocabinet-devel
     pip3 install glances
 }
