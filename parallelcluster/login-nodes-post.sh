ba920bf39510b259810e52972676076c3d1a44cd
Richard Vencu <vencu.richard@gmail.com>
2022-10-06 10:37:13 +0000

    slurmd is not needed on a login node

--- parallelcluster/login-nodes-post.sh
+++ parallelcluster/login-nodes-post.sh
@@ -38,30 +38,6 @@ export SLURM_HOME='/opt/slurm'
 export PATH=\$SLURM_HOME/bin:\$PATH
 EOF
 
-cat <<EOF > /etc/systemd/system/slurmd.service
-[Unit]
-Description=Slurm node daemon
-After=munge.service network.target remote-fs.target
-ConditionPathExists=/opt/slurm/etc/slurm.conf
-
-[Service]
-Type=simple
-EnvironmentFile=-/etc/sysconfig/slurmd
-ExecStart=/opt/slurm/sbin/slurmd -D \$SLURMD_OPTIONS
-ExecReload=/bin/kill -HUP \$MAINPID
-KillMode=process
-LimitNOFILE=131072
-LimitMEMLOCK=infinity
-LimitSTACK=infinity
-Delegate=yes
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-systemctl enable slurmd.service
-systemctl start slurmd.service
-
 # now logoff and make a new AMI from this node image
 # use the new AMI to spin up multiple login nodes
 # can also deregister the initial headnode AMI to save on expenses
