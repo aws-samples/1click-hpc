7a7f6ef38abb9a288038eed44f15ba17b5bae791
Richard Vencu <vencu.richard@gmail.com>
2022-10-31 19:16:03 +0000

    fix node version

--- scripts/Cloud9-Bootstrap.sh
+++ scripts/Cloud9-Bootstrap.sh
@@ -55,7 +55,7 @@ python3 -m pip install "aws-parallelcluster" --user --upgrade --quiet
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 chmod ug+x ~/.nvm/nvm.sh
 source ~/.nvm/nvm.sh > /dev/null 2>&1
-nvm install --lts > /dev/null 2>&1
+nvm install 17.9.1 > /dev/null 2>&1
 node --version
 
 
