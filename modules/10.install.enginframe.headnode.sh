2f5335ba756bc4447cc5d5ea4ab28c4e8f493a79
Nicola <venuti@amazon.com>
2022-11-08 18:40:26 +0000

    Moving to ParallelCluster 3.3

--- modules/10.install.enginframe.headnode.sh
+++ modules/10.install.enginframe.headnode.sh
@@ -26,6 +26,8 @@ set -e
 # ----------------------------------------------------------------------------
 installEnginFrame() {
     
+    amazon-linux-extras install -y java-openjdk11
+    
     wget -nv -P /tmp/packages https://dn3uclhgxk1jt.cloudfront.net/enginframe/packages/enginframe-latest.jar || exit 1
     
     aws s3 cp --quiet "${post_install_base}/enginframe/efinstall.config" /tmp/packages/ --region "${cfn_region}" || exit 1
@@ -40,7 +42,7 @@ installEnginFrame() {
         echo "[ERROR] missing efinstall.config" && return 1
 
     cat <<-EOF >> /tmp/packages/efinstall.config
-kernel.java.home = /usr/lib/jvm/java-1.8.0-openjdk/
+kernel.java.home = /usr/lib/jvm/jre-11/
 nice.root.dir.ui = ${NICE_ROOT}
 ef.spooler.dir = ${NICE_ROOT}/enginframe/spoolers/
 ef.repository.dir = ${NICE_ROOT}/enginframe/repository/
@@ -51,6 +53,7 @@ ef.temp.root.dir = ${NICE_ROOT}/enginframe/tmp/
 kernel.server.tomcat.https.ef.hostname = ${host_name}
 kernel.ef.db.admin.password = ${ec2user_pass}
 EOF
+
 
     # add EnginFrame users if not already exist
     id -u efnobody &>/dev/null || adduser efnobody
@@ -63,25 +66,14 @@ EOF
     
     # finally, launch EnginFrame installer
     ( cd /tmp/packages
-      /usr/lib/jvm/java-1.8.0-openjdk/bin/java -jar "${enginframe_jar}" --text --batch )
+      /usr/lib/jvm/jre-11/bin/java -jar "${enginframe_jar}" --text --batch )
 }
 
-createEnginFrameDB(){
+configureEnginFrameDB(){
     
     #FIXME: use latest link
     wget -nv -P "${EF_ROOT}/WEBAPP/WEB-INF/lib/" https://repo1.maven.org/maven2/mysql/mysql-connector-java/8.0.28/mysql-connector-java-8.0.28.jar
     chown ec2-user:efnobody "${EF_ROOT}/WEBAPP/WEB-INF/lib/mysql-connector-java-8.0.28.jar"
-
-    
-    aws s3 cp --quiet "${post_install_base}/enginframe/mysql/efdb.config" /tmp/ --region "${cfn_region}" || exit 1
-    aws s3 cp --quiet "${post_install_base}/enginframe/mysql/ef.mysql" /tmp/ --region "${cfn_region}" || exit 1
-    
-    export EF_DB_PASS="${ec2user_pass}"
-    /usr/bin/envsubst < efdb.config > efdb.pass.config
-    
-    mysql --defaults-extra-file="efdb.pass.config" < "ef.mysql"
-    rm efdb.pass.config efdb.config ef.mysql
-    
 }
 
 customizeEnginFrame() {
@@ -92,6 +84,9 @@ customizeEnginFrame() {
     sed -i \
         "s/^HY_CONNECT_SESSION_MAX_WAIT=.*$/HY_CONNECT_SESSION_MAX_WAIT='600'/" \
         "${EF_ROOT}/plugins/hydrogen/conf/ui.hydrogen.conf"
+        
+    #Fix DCV sessions not working with AD users
+    sed '2 i id "${USER}"' -i "${EF_ROOT}/plugins/interactive/lib/remote/linux.jobscript.functions"       
 }
 
 startEnginFrame() {
@@ -108,8 +103,8 @@ main() {
     unset EF_VERSION
     source "${EF_TOP}/current-version"
     export EF_ROOT="${EF_TOP}/${EF_VERSION}/enginframe"
-    createEnginFrameDB
     customizeEnginFrame
+    configureEnginFrameDB
     startEnginFrame
     echo "[INFO][$(date '+%Y-%m-%d %H:%M:%S')] 10.install.enginframe.headnode.sh: STOP" >&2
 }
