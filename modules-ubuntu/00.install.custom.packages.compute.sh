7e8a2d45f995189d74c8b0b73d7ce8200050d1b5
Richard Vencu <vencu.richard@gmail.com>
2023-06-10 13:08:47 +0000

    add python 3.12

--- modules-ubuntu/00.install.custom.packages.compute.sh
+++ modules-ubuntu/00.install.custom.packages.compute.sh
@@ -4,7 +4,12 @@ set -e
 
 installCustom() {
     apt-get -y update && apt-get -y upgrade
-    apt-get -y install wget tmux htop hwloc iftop aria2 numactl check subunit
+    apt-get -y install wget tmux htop hwloc iftop aria2 numactl check subunit python3.8-venv
+    add-apt-repository -y ppa:deadsnakes/ppa
+    apt-get -y update
+    apt-get -y install python3.12 python3.12-venv
+    rm /usr/bin/python3
+    ln -s /usr/bin/python3.12 /usr/bin/python3
     pip3 install glances
     apt-get -y remove postgres*
 }
--- modules-ubuntu/00.install.custom.packages.headnode.sh
+++ modules-ubuntu/00.install.custom.packages.headnode.sh
@@ -4,7 +4,12 @@ set -e
 
 installCustom() {
     apt -y update && apt -y upgrade
-    apt install -y tmux htop iftop transmission-cli pssh
+    apt install -y tmux htop iftop transmission-cli pssh python3.8-venv
+    add-apt-repository -y ppa:deadsnakes/ppa
+    apt-get -y update
+    apt-get -y install python3.12 python3.12-venv
+    rm /usr/bin/python3
+    ln -s /usr/bin/python3.12 /usr/bin/python3
     pip3 install glances
     apt -y remove postgres*
 }
