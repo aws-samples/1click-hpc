e78fde9be62dda421437c17b1083fcbbeafdd534
Richard Vencu <vencu.richard@gmail.com>
2023-06-15 13:02:17 +0000

    remove offending lib if necessary

--- modules-ubuntu/00.install.custom.packages.compute.sh
+++ modules-ubuntu/00.install.custom.packages.compute.sh
@@ -4,17 +4,18 @@ set -e
 
 installCustom() {
     apt-get -y update && apt-get -y upgrade
-    sudo add-apt-repository ppa:deadsnakes/ppa
-    sudo apt-get -q -o DPkg::Lock::Timeout=240 update
-    sudo apt-get -q -o DPkg::Lock::Timeout=240 install -y build-essential wget htop hwloc iftop aria2 numactl check subunit inotify-tools dotnet-sdk-7.0 bwm-ng subunit rustc cargo 
-    sudo apt-get -q -o DPkg::Lock::Timeout=240 install -y autoconf automake gdb git git-lfs libffi-dev zlib1g-dev ipset libsqlite3-dev libavcodec-dev libavfilter-dev libavformat-dev libavutil-dev
-    sudo apt-get -q -o DPkg::Lock::Timeout=240 install -y libssl-dev python3.8-venv libsndfile1 libsndfile1-dev ffmpeg libx264-dev libx265-dev logrotate
-    sudo apt-get -y remove apport
+    add-apt-repository ppa:deadsnakes/ppa
+    apt-get -q -o DPkg::Lock::Timeout=240 update
+    #apt-get purge -y ec2-instance-connect #required on ubuntu2004 https://github.com/widdix/aws-ec2-ssh/issues/157
+    apt-get -q -o DPkg::Lock::Timeout=240 install -y build-essential wget htop hwloc iftop aria2 numactl check subunit inotify-tools dotnet-sdk-7.0 bwm-ng subunit rustc cargo 
+    apt-get -q -o DPkg::Lock::Timeout=240 install -y autoconf automake gdb git git-lfs libffi-dev zlib1g-dev ipset libsqlite3-dev libavcodec-dev libavfilter-dev libavformat-dev libavutil-dev
+    apt-get -q -o DPkg::Lock::Timeout=240 install -y libssl-dev python3.8-venv libsndfile1 libsndfile1-dev ffmpeg libx264-dev libx265-dev logrotate
+    apt-get -y remove apport
     pip install --upgrade pip
-    sudo apt-get -q -oDPkg::Lock::Timeout=240 remove -y postgres*
+    apt-get -q -oDPkg::Lock::Timeout=240 remove -y postgres*
     #installing python versions
-    sudo apt-get -q -o DPkg::Lock::Timeout=240 install -y python3.9-venv python3.10-venv python3.11-venv python3.12-venv python3.9-dev python3.10-dev python3.11-dev python3.12-dev iotop iftop bwm-ng
-    sudo apt-get -q -o DPkg::Lock::Timeout=240 install -y python3.9-distutils python3.10-distutils python3.11-distutils
+    apt-get -q -o DPkg::Lock::Timeout=240 install -y python3.9-venv python3.10-venv python3.11-venv python3.12-venv python3.9-dev python3.10-dev python3.11-dev python3.12-dev iotop iftop bwm-ng
+    apt-get -q -o DPkg::Lock::Timeout=240 install -y python3.9-distutils python3.10-distutils python3.11-distutils
     pip3 install glances
 }
 
