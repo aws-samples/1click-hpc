3dd396d52947916fae383fee9793617b432f924d
Richard Vencu <vencu.richard@gmail.com>
2023-06-20 17:55:19 +0000

    knot resolver local ip ends in 53 for ubuntu

--- modules-ubuntu/00.install.knot.resolver.cpu.sh
+++ modules-ubuntu/00.install.knot.resolver.cpu.sh
@@ -18,7 +18,7 @@ installCustom() {
     G=$((${E}+2))
     vpcdns="${A}.${B}.${C}.${G}"
     
-    echo "supersede domain-name-servers 127.0.0.1, ${vpcdns};" >> /etc/dhcp/dhclient.conf
+    echo "supersede domain-name-servers 127.0.0.53, ${vpcdns};" >> /etc/dhcp/dhclient.conf
 
     echo "net.listen(net.lo, 8053, { kind = 'webmgmt' })" >> /etc/knot-resolver/kresd.conf
     echo "internalDomains = policy.todnames({'ec2.internal', 'us-west-2.compute.internal', '${shortstack_name}.pcluster','${cfn_region}.amazonaws.com'})" >> /etc/knot-resolver/kresd.conf
