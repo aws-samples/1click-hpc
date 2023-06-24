85aec8b24c6f0208520897fbeb52fcce93c4db4f
Richard Vencu <vencu.richard@gmail.com>
2023-06-24 14:34:05 +0000

    improve ohio

--- modules-ubuntu/00.install.custom.packages.compute.sh
+++ modules-ubuntu/00.install.custom.packages.compute.sh
@@ -5,7 +5,7 @@ set -e
 installCustom() {
     apt-get -y update && apt-get -y upgrade
     #apt-get purge -y ec2-instance-connect #required on ubuntu2004 https://github.com/widdix/aws-ec2-ssh/issues/157
-    apt-get -q -o DPkg::Lock::Timeout=240 install -y build-essential wget htop hwloc iftop aria2 numactl check subunit inotify-tools bwm-ng subunit rustc cargo 
+    apt-get -q -o DPkg::Lock::Timeout=240 install -y build-essential wget htop hwloc iftop aria2 numactl check subunit inotify-tools bwm-ng subunit rustc cargo nc
     apt-get -q -o DPkg::Lock::Timeout=240 install -y autoconf automake gdb git git-lfs libffi-dev zlib1g-dev ipset libsqlite3-dev libavcodec-dev libavfilter-dev libavformat-dev libavutil-dev
     apt-get -q -o DPkg::Lock::Timeout=240 install -y libssl-dev python3.8-venv libsndfile1 libsndfile1-dev ffmpeg libx264-dev libx265-dev logrotate openjdk-11-jre-headless
     apt-get -y remove apport
--- modules-ubuntu/00.install.custom.packages.headnode.sh
+++ modules-ubuntu/00.install.custom.packages.headnode.sh
@@ -6,7 +6,7 @@ installCustom() {
     apt-get -y update && apt-get -y upgrade
 
     #apt-get purge -y ec2-instance-connect #required on ubuntu2004 https://github.com/widdix/aws-ec2-ssh/issues/157
-    sudo apt-get -q -o DPkg::Lock::Timeout=240 install -y build-essential wget tmux htop hwloc iftop aria2 numactl check subunit inotify-tools bwm-ng subunit rustc cargo 
+    sudo apt-get -q -o DPkg::Lock::Timeout=240 install -y build-essential wget tmux htop hwloc iftop aria2 numactl check subunit inotify-tools bwm-ng subunit rustc cargo nc
     sudo apt-get -q -o DPkg::Lock::Timeout=240 install -y autoconf automake gdb git git-lfs libffi-dev zlib1g-dev ipset libsqlite3-dev libavcodec-dev libavfilter-dev libavformat-dev libavutil-dev
     sudo apt-get -q -o DPkg::Lock::Timeout=240 install -y libssl-dev python3.8-venv libsndfile1 libsndfile1-dev ffmpeg libx264-dev libx265-dev logrotate openjdk-11-jre-headless
     sudo apt-get -y remove apport
