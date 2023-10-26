f66aa6774b022dbe2c51d2ccc2ae9b8489f5be27
Richard Vencu <vencu.richard@gmail.com>
2023-10-26 12:32:54 +0000

    debug sudoers access

--- modules-ubuntu/10.install.proxy.iam.compute.sh
+++ modules-ubuntu/10.install.proxy.iam.compute.sh
@@ -13,7 +13,7 @@ installCustom() {
     systemctl start ica.service
     ln -s /opt/slurm/sbin/stablessh /usr/local/bin/stablessh
     #restrict ssh access only to Sudoers group
-    echo "AllowGroups Sudoers" >> /etc/ssh/sshd_config
+    #echo "AllowGroups Sudoers" >> /etc/ssh/sshd_config
     systemctl restart sshd
 }
 
