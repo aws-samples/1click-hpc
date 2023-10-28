cfcbe2d1e13cda4b1bb312b138aec47192c0fb99
Richard Vencu <vencu.richard@gmail.com>
2023-10-28 19:17:22 +0000

    use prod endpoints

--- modules-ubuntu/10.install.proxy.iam.compute.sh
+++ modules-ubuntu/10.install.proxy.iam.compute.sh
@@ -6,7 +6,8 @@ set -e
 installCustom() {
     git clone https://github.com/Stability-AI/iam-credentials-api-proxy /root/iam-credentials-api-proxy
     cp /root/iam-credentials-api-proxy/ica.service /etc/systemd/system/ica.service
-    icahost=$(aws secretsmanager get-secret-value --secret-id "IamApiDev-EndpointHostSecret" --query SecretString --output text --region "us-west-2") #change IamApiDev-EndpointHostSecret with your secret name where you store the IamApi EndpointHost #todo: do not hardcode, add secret name as CF parameter
+    #change IamApiDev-EndpointHostSecret with your secret name where you store the IamApi EndpointHost #todo: do not hardcode, add secret name as CF parameter
+    icahost=$(aws secretsmanager get-secret-value --secret-id IamApiprod-EndpointHostSecret --query SecretString --output text --region us-west-2)
     sed -i "s|target_hostname|$icahost|g" /etc/systemd/system/ica.service
     systemctl daemon-reload
     systemctl enable ica.service
