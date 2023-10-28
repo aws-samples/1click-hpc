c9073a06df14879cd52ec88fa6ab670837ab380b
Richard Vencu <vencu.richard@gmail.com>
2023-10-28 18:52:21 +0000

    reload

--- modules-ubuntu/00.install.custom.packages.compute.sh
+++ modules-ubuntu/00.install.custom.packages.compute.sh
@@ -17,6 +17,7 @@ installCustom() {
     echo net.ipv6.conf.all.disable_ipv6 = 1 >> /etc/sysctl.conf
     echo net.ipv6.conf.default.disable_ipv6 = 1 >> /etc/sysctl.conf
     echo net.ipv6.conf.lo.disable_ipv6 = 1 >> /etc/sysctl.conf
+    sysctl -p
 
     #installing python versions
     add-apt-repository ppa:deadsnakes/ppa -y
